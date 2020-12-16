2b47472aacae8f82ca9949eb7c5850f3d7ca67b7
ISStolyarov <68430258+ISStolyarov@users.noreply.github.com>
2020-12-16 16:35:13 +0300

    Update Сards.js

--- scripts/Сards.js
+++ scripts/Сards.js
@@ -26,3 +26,70 @@ const initialCards = [
 ];
 
 export { initialCards };
+
+
+class Card {
+    constructor(data, cardSelector) {
+      this._image = data.link;
+      this._title = data.name;
+      this._cardSelector = cardSelector;
+    }
+  
+    _getTemplate() {
+      const cardElement = document
+        .querySelector(this._cardSelector)
+        .content
+        .querySelector('.card')
+        .cloneNode(true);
+  
+      return cardElement;
+    }
+  
+    generateCard() {
+      this._element = this._getTemplate();
+  
+      this._setEventListeners();
+  
+      const image = this._element.querySelector('.card__image');
+  
+      this._element.querySelector('.card__image').src = this._image;
+      this._element.querySelector('.card__title').textContent = this._title;
+  
+      image.src = this._image;
+      image.alt = this._title;
+  
+      return this._element;
+    }
+  
+    _setEventListeners() {
+      this._element.querySelector('.card__heart').addEventListener('click', () => {
+        this._handleHeartClick();
+      });
+  
+      this._element.querySelector('.card__trash').addEventListener('click', () => {
+        this._handleTrashClick();
+      });
+  
+      this._element.querySelector('.card__image').addEventListener('click', () => {
+        this._handleOpenPopupZoom();
+      });
+  
+    };
+  
+    _handleHeartClick() {
+      this._element.querySelector('.card__heart').classList.toggle('card__heart_active');
+    };
+  
+    _handleTrashClick() {
+      this._element.querySelector('.card__trash').closest('.card').remove();
+    };
+  
+    _handleOpenPopupZoom() {
+      popupPhoto.src = this._image;
+      popupTitlePhoto.textContent = this._title;
+      showPopup(popupZoom);
+    };
+  
+  };
+
+  export { Card };
