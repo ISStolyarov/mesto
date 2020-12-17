5b97a816254e015ff203841b59afa267a8b4ceca
ISStolyarov <68430258+ISStolyarov@users.noreply.github.com>
2020-12-17 10:33:13 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,6 @@ export const popupTitlePhoto = document.querySelector(".popup__caption");
 export const popupPhoto = document.querySelector(".popup__photo");
 const cardTitle = document.querySelector(".popup__input_add_title");
 const cardImage = document.querySelector(".popup__input_add_image");
-// const cardTemplate = document.querySelector("#card-template").content;
 
 const validationConfig = {
   formSelector: ".popup__form",
@@ -29,13 +28,20 @@ const validationConfig = {
   inputInvalidClass: "popup__input_state_invalid",
   buttonInvalidClass: "popup__button_invalid",
 };
- 
+
 const formEditValidation = new FormValidation(validationConfig, formEdit);
 const formAddValidation = new FormValidation(validationConfig, formAddCard);
 
 formEditValidation.enableValidation();
 formAddValidation.enableValidation();
 
+initialCards.forEach(item => addCard(item));
+
+function addCard(item) {
+  const card = new Card(item, '.card-template');
+  const cardElement = card.generateCard();
+  cardSection.prepend(cardElement);
+}
 
 export function showPopup(popup) {
   popup.classList.add("popup_opened");
@@ -62,114 +68,6 @@ const closePopupMouse = (evt) => {
   }
 };
 
-// class Card {
-//   constructor(data, cardSelector) {
-//     this._image = data.link;
-//     this._title = data.name;
-//     this._cardSelector = cardSelector;
-//   }
-
-//   _getTemplate() {
-//     const cardElement = document
-//       .querySelector(this._cardSelector)
-//       .content
-//       .querySelector('.card')
-//       .cloneNode(true);
-
-//     return cardElement;
-//   }
-
-//   generateCard() {
-//     this._element = this._getTemplate();
-
-//     this._setEventListeners();
-
-//     const image = this._element.querySelector('.card__image');
-
-//     this._element.querySelector('.card__image').src = this._image;
-//     this._element.querySelector('.card__title').textContent = this._title;
-
-//     image.src = this._image;
-//     image.alt = this._title;
-
-//     return this._element;
-//   }
-
-//   _setEventListeners() {
-//     this._element.querySelector('.card__heart').addEventListener('click', () => {
-//       this._handleHeartClick();
-//     });
-
-//     this._element.querySelector('.card__trash').addEventListener('click', () => {
-//       this._handleTrashClick();
-//     });
-
-//     this._element.querySelector('.card__image').addEventListener('click', () => {
-//       this._handleOpenPopupZoom();
-//     });
-
-//   };
-
-//   _handleHeartClick() {
-//     this._element.querySelector('.card__heart').classList.toggle('card__heart_active');
-//   };
-
-//   _handleTrashClick() {
-//     this._element.querySelector('.card__trash').closest('.card').remove();
-//   };
-
-//   _handleOpenPopupZoom() {
-//     popupPhoto.src = this._image;
-//     popupTitlePhoto.textContent = this._title;
-//     showPopup(popupZoom);
-//   };
-
-// };
-
-initialCards.forEach((item) => {
-  const card = new Card(item, '.card-template');
-  const cardElement = card.generateCard();
-
-  cardSection.append(cardElement);
-});
-
-
-// function createCard(card) {
-//   const initCard = cardTemplate.cloneNode(true);
-//   const image = initCard.querySelector(".card__image");
-//   const title = initCard.querySelector(".card__title");
-//   const heartActive = initCard
-//     .querySelector(".card__heart")
-//     .addEventListener("click", function (evt) {
-//       evt.target.classList.toggle("card__heart_active");
-//     });
-
-//   image.addEventListener("click", (event) => {
-//     popupPhoto.src = card.link;
-//     popupTitlePhoto.textContent = card.name;
-//     popupTitlePhoto.alt = card.name;
-//     showPopup(popupZoom);
-//   });
-
-//   initCard.querySelector(".card__trash").addEventListener("click", (event) => {
-//     const delCard = event.target.closest(".card");
-
-//     delCard.remove();
-//   });
-
-//   image.src = card.link;
-//   image.alt = card.name;
-//   title.textContent = card.name;
-
-//   return initCard;
-// }
-
-// function addCard(card) {
-//   cardSection.prepend(createCard(card));
-// }
-
-// initialCards.forEach((card) => addCard(card, cardSection));
-
 function showEditUserPopup() {
   showPopup(popupEdit);
   formName.value = nameInput.textContent;
@@ -186,7 +84,7 @@ formAddCard.addEventListener("submit", (evt) => {
   addCard({
     name: cardTitle.value,
     link: cardImage.value,
-  }, cardSection, true);
+  });
 
   formAddCard.reset();
   closePopup(popupAdd);
